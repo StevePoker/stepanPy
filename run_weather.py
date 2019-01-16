c559e4ccd19bba8c9e54d8e4e451e67f6a8c5ce8
stevepoker <stevepokersx@gmail.com>
2019-01-16 23:36:13 +0200

    run_weather last update

--- run_weather.py
+++ run_weather.py
@@ -5,12 +5,11 @@ class PageParser(object):
    def __init__(self, url):
        self.url = url
 
-   def weather(self):
+   def temperature(self):
        response = requests.get(self.url)
        soup = BeautifulSoup(response.text, 'html.parser')
        tag_selected = soup.select('.today-temp')
        print(tag_selected[0].text)
-       return tag_selected[0].text
 
-temperature = PageParser("https://sinoptik.ua/погода-киев")
-temperature.weather()
+weather = PageParser("https://sinoptik.ua/погода-киев")
+weather.temperature()
