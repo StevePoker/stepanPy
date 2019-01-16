f4f9da42797130301312a727d8cd6385bdbd85b3
stevepoker <stevepokersx@gmail.com>
2019-01-16 14:31:52 +0200

    update weather in classes

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_html(url):
-    response = requests.get(url)
-    return response.text
+page = "https://sinoptik.ua/погода-киев"
 
-html_text = get_html("https://sinoptik.ua/погода-киев")
-site = BeautifulSoup(html_text, "html.parser")
-temper = site.select('.today-temp')
-weather = temper[0].getText()
-print("In Kiev now: " + weather)
+class sinoptik():
+    def weather(self):
+        response = requests.get(page)
+        site = BeautifulSoup(response.text, "html.parser")
+        temper = site.select('.today-temp')
+        weather_now = temper[0].text
+        print(weather_now)
+        return weather_now
+
+temperature = sinoptik()
+temperature.weather()
