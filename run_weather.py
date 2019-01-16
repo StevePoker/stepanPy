4187c8be5326d6805296a4fba633009cef98c91a
stevepoker <stevepokersx@gmail.com>
2019-01-16 17:20:37 +0200

    update 2-nd solution

--- run_weather.py
+++ run_weather.py
@@ -3,13 +3,13 @@ from bs4 import BeautifulSoup
 
 class PageParser(object):
    def __init__(self, url):
-        self.response = requests.get(url)
+       self.response = requests.get(url)
 
    def weather(self):
-    soup = BeautifulSoup(self.response.text, 'html.parser')
-    tag_selected = soup.select('.today-temp')
-    print(tag_selected[0].text)
-    return tag_selected[0].text
+       soup = BeautifulSoup(self.response.text, 'html.parser')
+       tag_selected = soup.select('.today-temp')
+       print(tag_selected[0].text)
+       return tag_selected[0].text
 
 temperature = PageParser("https://sinoptik.ua/погода-киев")
 temperature.weather()
